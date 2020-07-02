ec011956a1fa731f588803e7ac6ebe3fa2767dc8
Jaewoo <jaewoo683@gmail.com>
2020-07-02 11:51:16 +0900

    cors 설정 2

--- src/main/java/com/recommendation/FRS/config/WebConfig.java
+++ src/main/java/com/recommendation/FRS/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 공동 작업하는 경우 ip 체크 필요
         registry.addMapping("/**")
+                .allowCredentials(true)
                 .allowedOrigins("http://172.31.19.223:3001")
                 .allowedOrigins("http://172.25.231.51:3000") // 지은
                 .allowedOrigins("http://172.31.16.31:3000") // 원종
